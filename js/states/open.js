b8508194de2a0f0941bb2e0ccb7fc77ae250567d
Kaizena Ma <kma39@ucsc.edu>
2019-05-20 18:19:01 -0700

    started working on opening narration sequence

---
+++ js/states/open.js
@@ -0,0 +1,20 @@
+//Group 27
+//Kaizena Ma
+//Holly Cheng
+//Linda Xieu
+
+//Opening Narration State
+"use strict";
+
+var Open = function(game){};
+Open.prototype = {
+	init: function(){
+
+	},
+	create: function(){
+
+	},
+	updare: function(){
+
+	}
+}
